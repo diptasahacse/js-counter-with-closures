f529250a5b637109e77469c6ba0a8cefa2f4f466
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-03-13 16:57:37 +0600

    -

---
+++ script.js
@@ -0,0 +1,20 @@
+let output = document.getElementById("output");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+
+const add = (function () {
+  let counter = 0;
+  return function (a) {
+    console.log(a)
+    a == "plus" ? counter += 1 : counter -= 1
+    return counter;
+  };
+})();
+
+
+plus.addEventListener("click", () => {
+  document.getElementById("output").innerHTML = add('plus');
+});
+minus.addEventListener("click", () => {
+  document.getElementById("output").innerHTML = add('minus');
+});
