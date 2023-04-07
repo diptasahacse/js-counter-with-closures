273d8ee64c934c862dae59512799cc386b81ce59
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2023-04-08 03:28:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ let output = document.getElementById("output");
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
 
-const add = (function () {
-  let counter = 0;
+const counter = (function () {
+  let count = 0;
   return function (operation) {
-    operation == "plus" ? counter += 1 : counter -= 1;
-    return counter;
+    operation == "plus" ? count += 1 : count -= 1;
+    return count;
   };
 })();
 
 
 plus.addEventListener("click", () => {
-  document.getElementById("output").innerHTML = add('plus');
+  document.getElementById("output").innerHTML = counter('plus');
 });
 minus.addEventListener("click", () => {
-  document.getElementById("output").innerHTML = add('minus');
+  document.getElementById("output").innerHTML = counter('minus');
 });
